fe42909f49164b764f075a14687d683ff2a9bf54
Joshua Hecker <jhecker@gmail.com>
2013-04-04 20:27:01 -0600

    AppDelegate -Default FILE_PATH variable now set to ~/Desktop to resolve user-specific issues -Refactored "sim" variable to "simulation" (now agrees with AntBot-GA-CLI)

--- AntBot-GA-GUI/AppDelegate.m
+++ AntBot-GA-GUI/AppDelegate.m
@@ -1,6 +1,6 @@
 #import "AppDelegate.h"
 
-NSString *FILE_PATH = @"~/Dropbox/School/Research/AntBot/Data/GA evolved parameters";
+NSString *FILE_PATH = @"~/Desktop";
 int NUM_ITERATIONS = 10;
 
 @implementation AppDelegate
@@ -9,32 +9,32 @@ int NUM_ITERATIONS = 10;
 
 -(void) applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    Simulation* sim = [[Simulation alloc] init];
+    Simulation* simulation = [[Simulation alloc] init];
     
-    [sim setRobotCount:6];
-    [sim setTeamCount:100];
-    [sim setGenerationCount:100];
-    [sim setTagCount:256];
-    [sim setEvaluationCount:1];
+    [simulation setRobotCount:6];
+    [simulation setTeamCount:100];
+    [simulation setGenerationCount:100];
+    [simulation setTagCount:256];
+    [simulation setEvaluationCount:1];
     
-    [sim setDistributionClustered:0.];
-    [sim setDistributionPowerlaw:1.];
-    [sim setDistributionRandom:0.];
+    [simulation setDistributionClustered:0.];
+    [simulation setDistributionPowerlaw:1.];
+    [simulation setDistributionRandom:0.];
     
-    [sim setPositionalError:0.f];
-    [sim setDetectionError:0.f];
+    [simulation setPositionalError:0.f];
+    [simulation setDetectionError:0.f];
     
-    [sim setRandomizeParameters:TRUE];
+    [simulation setRandomizeParameters:TRUE];
     
-    [sim setDelegate:self];
-    [sim setTickRate:.005f];
-    [sim setViewDelegate:(NSObject*)simView];
+    [simulation setDelegate:self];
+    [simulation setTickRate:.005f];
+    [simulation setViewDelegate:(NSObject*)simView];
     
-    [sim setParameterFile:[NSString stringWithFormat:@"%@/parameters.csv",[FILE_PATH stringByExpandingTildeInPath]]];
+    [simulation setParameterFile:[NSString stringWithFormat:@"%@/parameters.csv",[FILE_PATH stringByExpandingTildeInPath]]];
     
     dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
     dispatch_async(queue, ^{
-        [sim start];
+        [simulation start];
     });
 }
 
