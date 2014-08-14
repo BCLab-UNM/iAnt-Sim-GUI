bdc4f606bfb47bdb160b397c6aec959238545311
Joshua Hecker <jhecker@gmail.com>
2014-08-14 15:48:27 -0600

    SimulationView -Class switched to Objective-C++ to handle STL grid vector from Simulation -Grid is now a two-dimensional STL vector -QuadTree drawing updated to show NSRect regions -Pheromone drawing updated to show decentralized pheromones (if enabled)

--- AntBot-GA-GUI/SimulationView.h
+++ AntBot-GA-GUI/SimulationView.h
@@ -5,13 +5,16 @@
     NSTimer* drawTimer;
 }
 
--(void) updateDisplayWindowWithRobots:(NSMutableArray*)_robots team:(Team*)_team grid:(Array2D*)_grid pheromones:(NSMutableArray*)_pheromones regions:(NSMutableArray*)_regions clusters:(NSMutableArray *)_clusters;
+#ifdef __cplusplus
+-(void) updateDisplayWindowWithRobots:(NSMutableArray*)_robots team:(Team*)_team grid:(std::vector<std::vector<Cell*> >)_grid pheromones:(NSMutableArray*)_pheromones regions:(NSMutableArray*)_regions clusters:(NSMutableArray *)_clusters;
+@property (nonatomic) std::vector<std::vector<Cell*>> grid;
+#endif
+
 -(void) redraw;
 
 @property (nonatomic) Simulation* simulation;
 @property (nonatomic) NSMutableArray* robots;
 @property (nonatomic) Team* team;
-@property (nonatomic) Array2D* grid;
 @property (nonatomic) NSMutableArray* pheromones;
 @property (nonatomic) NSMutableArray* regions;
 @property (nonatomic) NSMutableArray* clusters;
